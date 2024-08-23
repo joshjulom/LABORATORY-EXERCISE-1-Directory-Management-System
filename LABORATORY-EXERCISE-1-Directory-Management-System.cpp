0b9c79534d277e8e2776a0afc183241b1e3e6717
joshjulom <joshjulom@gmail.com>
2024-08-24 00:48:20 +0800

    #include <iostream> #include <cstdio>     // For system("pause") #include <direct.h>   // For _mkdir and _chdir  void displayFiles() {     system("dir");  // Lists files in the current directory (Windows command)     system("pause");  // Waits for user to press a key }

--- LABORATORY-EXERCISE-1-Directory-Management-System.cpp
+++ LABORATORY-EXERCISE-1-Directory-Management-System.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <cstdio>     // For system("pause")
-#include <cstdlib>    // For system("cls")
 #include <direct.h>   // For _mkdir and _chdir
-#include <windows.h>  // For _getcwd and to handle directories
 
 void displayFiles() {
     system("dir");  // Lists files in the current directory (Windows command)
