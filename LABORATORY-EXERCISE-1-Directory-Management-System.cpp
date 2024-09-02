d135897332c7dc731d185745aad40eb5b2263225
joshjulom <joshjulom@gmail.com>
2024-09-02 11:30:22 +0800

    New Update Latest

--- LABORATORY-EXERCISE-1-Directory-Management-System.cpp
+++ LABORATORY-EXERCISE-1-Directory-Management-System.cpp
@@ -5,10 +5,10 @@
 
 using namespace std;
 
-void listFiles(const string& path) {
+void listFiles(const string& path, const string& pattern = "*.*") {
     cout << "Listing files in directory: " << path << endl;
     WIN32_FIND_DATA findFileData;
-    HANDLE hFind = FindFirstFile((path + "/*").c_str(), &findFileData);
+    HANDLE hFind = FindFirstFile((path + "\\" + pattern).c_str(), &findFileData);
 
     if (hFind == INVALID_HANDLE_VALUE) {
         cout << "Error: Could not open directory." << endl;
@@ -16,7 +16,11 @@ void listFiles(const string& path) {
     }
 
     do {
-        cout << findFileData.cFileName << endl;
+        if (findFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
+            cout << "[DIR] " << findFileData.cFileName << endl;
+        } else {
+            cout << findFileData.cFileName << endl;
+        }
     } while (FindNextFile(hFind, &findFileData) != 0);
 
     FindClose(hFind);
@@ -42,11 +46,29 @@ void changeDirectory(string& currentPath, const string& newPath) {
 }
 
 void showMenu() {
-    cout << "\nMenu:\n";
+    cout << "\nMain Menu:\n";
     cout << "1. List Files\n";
     cout << "2. Create Directory\n";
     cout << "3. Change Directory\n";
     cout << "4. Exit\n";
+    cout << "Enter your choice: ";
+}
+
+void showListFilesMenu() {
+    cout << "\nList Files Menu:\n";
+    cout << "1. List all files\n";
+    cout << "2. List files by extension\n";
+    cout << "3. List files by pattern\n";
+    cout << "4. Back to main menu\n";
+    cout << "Enter your choice: ";
+}
+
+void showChangeDirectoryMenu() {
+    cout << "\nChange Directory Menu:\n";
+    cout << "1. Move one step back (to the parent directory)\n";
+    cout << "2. Move to the root directory\n";
+    cout << "3. Move to a specific directory\n";
+    cout << "4. Back to main menu\n";
     cout << "Enter your choice: ";
 }
 
@@ -65,7 +87,36 @@ int main() {
 
         switch (choice) {
         case 1: {
-            listFiles(currentPath);
+            int listChoice;
+            do {
+                showListFilesMenu();
+                cin >> listChoice;
+
+                switch (listChoice) {
+                case 1:
+                    listFiles(currentPath);
+                    break;
+                case 2: {
+                    string extension;
+                    cout << "Enter the file extension (e.g., .txt): ";
+                    cin >> extension;
+                    listFiles(currentPath, "*"+extension);
+                    break;
+                }
+                case 3: {
+                    string pattern;
+                    cout << "Enter the file pattern (e.g., moha*.*): ";
+                    cin >> pattern;
+                    listFiles(currentPath, pattern);
+                    break;
+                }
+                case 4:
+                    break;
+                default:
+                    cout << "Invalid choice. Please try again." << endl;
+                    break;
+                }
+            } while (listChoice != 4);
             break;
         }
         case 2: {
@@ -76,20 +127,42 @@ int main() {
             break;
         }
         case 3: {
-            string newPath;
-            cout << "Enter the path to change to: ";
-            cin >> newPath;
-            changeDirectory(currentPath, newPath);
+            int changeChoice;
+            do {
+                showChangeDirectoryMenu();
+                cin >> changeChoice;
+
+                switch (changeChoice) {
+                case 1: {
+                    string parentDir = currentPath.substr(0, currentPath.find_last_of("\\"));
+                    changeDirectory(currentPath, parentDir.empty() ? "." : parentDir);
+                    break;
+                }
+                case 2:
+                    changeDirectory(currentPath, "C:\\"); // Root directory
+                    break;
+                case 3: {
+                    string newPath;
+                    cout << "Enter the path to change to: ";
+                    cin >> newPath;
+                    changeDirectory(currentPath, newPath);
+                    break;
+                }
+                case 4:
+                    break;
+                default:
+                    cout << "Invalid choice. Please try again." << endl;
+                    break;
+                }
+            } while (changeChoice != 4);
             break;
         }
-        case 4: {
+        case 4:
             cout << "Exiting..." << endl;
             break;
-        }
-        default: {
+        default:
             cout << "Invalid choice. Please try again." << endl;
             break;
-        }
         }
 
     } while (choice != 4);
